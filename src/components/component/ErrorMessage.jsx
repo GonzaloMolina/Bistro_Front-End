30d3e47e5405ac086468491ee706c29f581f62a4
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2023-09-19 19:59:13 -0300

    nueva view de la table

--- src/components/component/ErrorMessage.jsx
+++ src/components/component/ErrorMessage.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const ErrorMessage = props => {
     return (
       props.error
-        ? <div className="alert alert-danger" role="alert">{props.error}</div>
+        ? <div className="alert alert-danger" role="alert" style={{textAlign: 'center', zIndex: 'inherit'}}>
+            {props.error}
+          </div>
         : null
     );
   }
