64e03c31b6d67b67a3b239f605e7046605eb9633
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2023-09-20 19:42:56 -0300

    se mantiene un estado entre los diferentes componentes

--- src/components/component/CheckStep.jsx
+++ src/components/component/CheckStep.jsx
@@ -2,11 +2,32 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 class CheckStep extends React.Component {
-    render(){console.log(this.props);
+    constructor(props){
+        super(props)
+        this.state={
+            orden: this.props.orden()
+        }
+    }
+
+    checkList(){
+        var temp =this.props.orden()
+        temp.push(
+            {
+                'plate':this.props.plate(),
+                'cantidad': this.props.cant()
+            }
+        );
+        console.log(temp);
+        return temp;
+    }
+
+    render(){
         return(
             <React.Fragment>
                 <div className='check-container'>
-                    CheckStep
+                    {this.checkList().map((e,i) => {
+                        return <p key={i}>{e.plate.nombre +"  "+ e.cantidad} </p>
+                    })}    
                 </div>
             </React.Fragment>
         );
