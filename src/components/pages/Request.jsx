cd09f7bc3b5d3622569cdfa98ba56791b5dad68f
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2023-10-11 21:44:01 -0300

    mod: Request ahora muestra el listado de solicitudes y el boton para redactar

--- src/components/pages/Request.jsx
+++ src/components/pages/Request.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {withRouter} from 'react-router';
 import Sidebar from '../component/Sidebar';
 import logo from "../img/bistrot.jpg";
+import { BsCheck2Square } from 'react-icons/bs';
+import { GrCheckbox } from 'react-icons/gr';
+import { AiOutlineCloseSquare } from 'react-icons/ai';
+//import '../../styles/table.css'
 
 class Request extends React.Component {
     constructor(props){
         super(props);
         this.state = { 
-            nombre: "",
-            apellido: "",
             email: "",
             password: "",
             mesas: [],
@@ -19,19 +21,56 @@ class Request extends React.Component {
     componentDidMount(){
         if(this.props.user === undefined){this.props.history.push('/')}
         else{
-            this.setState(state => ({nombre: !this.props.user.nombre}));
-            this.setState(state => ({nombre: this.props.user.nombre}));
-            this.setState(state => ({apellido: !this.props.user.apellido}));
-            this.setState(state => ({apellido: this.props.user.apellido}));
             this.setState(state => ({email: !this.props.user.email}));
             this.setState(state => ({email: this.props.user.email}));
-            this.setState(state => ({password: !this.props.user.password}));
-            this.setState(state => ({password: this.props.user.password}));
+            this.setState(state => ({password: !this.props.user.pass}));
+            this.setState(state => ({password: this.props.user.pass}));
             this.setState(state => ({mesas: !this.props.user.mesas}));
             this.setState(state => ({mesas: this.props.user.mesas}));
             this.setState(state => ({solicitudes: !this.props.user.peticiones}));
             this.setState(state => ({solicitudes: this.props.user.peticiones}));
         }
+    }
+
+    renderEstado = (state) => state==='ACEPTADA'?  this.greenB() : state==='RECHAZADA'?  this.redB() : this.blackB()
+
+    greenB = () => {
+        return (
+        <button type='button' className='btn'
+        style={{
+            color: 'green',
+            fontSize: '20px',
+            cursor: 'default'
+        }}>
+            <BsCheck2Square/>
+        </button>
+        )
+    }
+
+    redB = () => {
+        return (
+        <button type='button' className='btn'
+        style={{
+            color: 'red',
+            fontSize: '20px',
+            cursor: 'default'
+        }}>
+            <AiOutlineCloseSquare/>
+        </button>
+        )
+    }
+
+    blackB = () => {
+        return (
+        <button type='button' className='btn'
+        style={{
+            color: 'red',
+            fontSize: '20px',
+            cursor: 'default'
+        }}>
+            <GrCheckbox/>
+        </button>
+        )
     }
 
     render(){
@@ -54,7 +93,54 @@ class Request extends React.Component {
                     />
                 </div>
 
+                <div className='card' 
+                    style={{
+                        marginTop: '2%',
+                        marginLeft:'5%', 
+                        marginRight:'5%', 
+                        zIndex: '0', 
+                        backgroundColor: 'rgba(179, 241, 178, 0.4)', 
+                        borderRadius:'20px'
+                    }}
+                >
+                    <ul style={{ 
+                        listStyleType: 'none',
+                    }}>
+                    {this.state.solicitudes.map((sol, i) => {
+                        return (
+                            <li>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <div classname='col' style={{}}>
+                                            <span> {sol.asunto} </span> 
+                                        </div>
+                                        <div classname='col'>
+                                            {this.renderEstado(sol.estado)}
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                    </ul>
+                </div>
 
+                <div className="container">
+                    <div className='btn-holder' 
+                    style={{
+                        bottom: '15%',
+                        right: '8%'
+                    }}>
+                        <div className="">
+                            <button 
+                                className="btn btn-secondary"
+                                onClick={() => console.log('a')}
+                            >
+                                Redactar
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
