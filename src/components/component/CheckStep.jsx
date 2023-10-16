52077ba8c44b2f3241a17ae477c1d896e47c89d4
AxelLopezGarabal <axel.lopez.garabal@gmail.com>
2023-10-16 09:41:59 -0300

    mod: render de los platos

--- src/components/component/CheckStep.jsx
+++ src/components/component/CheckStep.jsx
@@ -20,7 +20,7 @@ class CheckStep extends React.Component {
         const selected = this.props.elem().elem;console.log("elem:", selected);
         const cantidad = this.props.elem().amount;
         const values = this.props.elem().values;console.log("values: ",values);
-        this.createBody(selected, cantidad, values)
+        this.createBody(selected, cantidad, values);
         this.stateView(selected, cantidad, values)
     }
 
@@ -42,16 +42,20 @@ class CheckStep extends React.Component {
         }
     }
 
+    belongs(elem, ls){
+        return ls.filter(e => e.id !== elem.id).length > 1;
+    }
+
     stateView(selected, cantidad, values){
-        if(this.props.elem().elem.tamanio === undefined){
-            let tempView = {
-                "key": selected,
-                "values": values
+        if(selected.tamanio === undefined){
+            if(!(this.belongs(selected, this.props.getLs[2]()))){
+                let tempView = {
+                    "key": selected,
+                    "values": values
+                }
+                this.props.setLs[2](this.props.getLs[2]().concat([tempView]));
+                this.setState(state => ({platosView: this.props.getLs[2]().concat(tempView)}));
             }
-            this.props.setLs[2](this.props.getLs[2]().concat([tempView]));
-            this.setState(state => ({platosView: this.props.getLs[2]().concat(tempView)}));
-            console.log(this.state)
-            console.log(this.props.getLs[2]())
         }else{
             let tempView = {
                 "key": selected,
@@ -65,118 +69,149 @@ class CheckStep extends React.Component {
     renderAcompañamiento(pla,i){
         if(pla.values.length === 0){
             return (
-                <div className='card'
-                    style={{
-                        zIndex: '0', 
-                        backdropFilter: 'blur(10px)', 
-                        backgroundColor:'rgba(179, 241, 178, 0.5)',
-                        margin: '30px',
-                        marginTop:'0px',
-                        marginBottom:'0px'
-                    }}
-                >
-                    <li span className='nav-text'>
-                        <span style={{ fontFamily: 'Cinzel' }}> {pla.key.nombre}</span>
-                        <div style={{overflow: 'auto'}}>
-                            <button 
-                                type='button'
-                                className='btn'
-                                onClick={() => {
-                                    console.log(this.state)
-                                }}
+                <div key={i} className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" 
+                        style={{flexDirection: 'column', marginTop: '5px'}}>
+                    <div className="btn-group mr-2 flex-wrap" role="group" aria-label="First group"
+                            style={{marginBottom:'0px'}}>
+                        <button type='button' className='btn btn-secondary'
                                 style={{
-                                    color: 'red',
-                                    float:'right'
+                                    borderRadius: '10px',
+                                    borderTopLeftRadius:'30px',
+                                    borderBottomLeftRadius:'30px',
+                                    marginBottom: '0px', height: '50px',
+                                    backdropFilter: 'blur(10px)', backgroundColor:'rgba(179, 241, 178, 0.5)',
+                                    color: 'black'
                                 }}
                             >
-                                <AiOutlineCloseSquare/>
-                            </button>
-                        </div>
-                    </li>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <span>{pla.key.nombre}</span>
+                                    </div>
+                                    <div class="col">
+                                        <span>{''}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </button>
+                            {this.renderDBotton(pla)}
+                    </div>
                 </div>
             )
         } else{
             return pla.values.map((acom, k) => {
                 return (
-                    <div className='card'
-                        style={{
-                            zIndex: '0', 
-                            backdropFilter: 'blur(10px)', 
-                            backgroundColor:'rgba(179, 241, 178, 0.5)',
-                            margin: '30px',
-                            marginTop:'0px',
-                            marginBottom:'0px'
-                        }}
-                    >
-                        <li key={k} span className='nav-text'>
-                            <span style={{ fontFamily: 'Cinzel' }}> {pla.key.nombre}</span>
-                            <span style={{ fontFamily: 'Cinzel' }}> {acom.nombre}</span>
-                            <div style={{overflow: 'auto'}}>
-                                <button 
-                                    type='button'
-                                    className='btn'
-                                    onClick={() => {
-                                        console.log(this.state)
-                                    }}
+                    <div key={k} className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" 
+                        style={{flexDirection: 'column', marginTop: '5px'}}>
+                        <div className="btn-group mr-2" role="group" aria-label="First group"
+                            style={{marginBottom:'0px'}}>
+                            <button type='button' className='btn btn-secondary'
                                     style={{
-                                        color: 'red',
-                                        float:'right'
+                                        borderRadius: '10px',
+                                        borderTopLeftRadius:'30px',
+                                        borderBottomLeftRadius:'30px',
+                                        marginBottom: '0px', height: 'auto',
+                                        backdropFilter: 'blur(10px)', backgroundColor:'rgba(179, 241, 178, 0.5)',
+                                        color: 'black'
                                     }}
                                 >
-                                    <AiOutlineCloseSquare/>
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-4" align='left'>
+                                                <p style={{fontSize:''}}>{pla.key.nombre}</p>
+                                            </div>
+                                            <div className="col-5" align='left'>
+                                                <p style={{fontSize:''}}>{acom.nombre}</p>
+                                            </div>
+                                            <div className="col-1" align='right' style={{}}>
+                                                <span style={{fontSize:''}}>x{pla.values.length}</span>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </button>
-                            </div>
-                        </li>
+                            
+                                {this.renderDBotton(pla)}
+                        </div>
                     </div>
                 )
-        })
+            })
+        }
     }
-}
+
+    renderDBotton(b){
+        return (
+            <button type='button' className='btn-secondary'
+                onClick={() => {console.log(b);}}
+                style={{zIndex:'0', borderRadius: '10px', 
+                    border:'0px',
+                    marginRight:'5px', 
+                    marginLeft:'2px',
+                    borderTopRightRadius:'30px',
+                    borderBottomRightRadius:'30px',
+                    backdropFilter: 'blur(10px)', 
+                    backgroundColor:'rgba(179, 241, 178, 0.5)',
+                }}
+            >
+                <AiOutlineCloseSquare 
+                color='red'
+                fontSize='1.5em'
+                style={{zIndex:'0', 
+                    borderRadius: '10px', 
+                    margin:'0px'
+                }}
+            />
+            </button>
+        )
+    }
+
+    renderBebida(bebida){
+        return (
+            <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups" 
+                style={{flexDirection: 'column', marginTop: '5px'}}>
+                <div className="btn-group mr-2 flex-wrap" role="group" aria-label="First group"
+                    style={{marginBottom:'0px'}}>
+                    <button type='button' className='btn btn-secondary'
+                            style={{
+                                borderRadius: '10px',
+                                borderTopLeftRadius:'30px',
+                                borderBottomLeftRadius:'30px',
+                                marginBottom: '0px', height: '50px',
+                                backdropFilter: 'blur(10px)', backgroundColor:'rgba(179, 241, 178, 0.5)',
+                                color: 'black'
+                            }}
+                        >
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-8">
+                                        <span>{bebida.key.nombre}</span>
+                                    </div>
+                                    <div class="col">
+                                        <span>x{bebida.amount}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </button>
+                    
+                        {this.renderDBotton(bebida)}
+                </div>
+            </div>
+        )
+    }
 
     render(){
         return(
             <React.Fragment>
-                <div className='check-container'>
-                <IconContext.Provider value={{ className: "shared-class", size: 40 }}>
+                <div className='check-container' style={{margin:'10px', marginRight: '20px', marginLeft:'20px'}}>
+                <IconContext.Provider value={{ className: "shared-class", size: 20 }}/>
                     {this.props.getLs[2]().map((pla, i) => {
                         console.log(pla)
                         return this.renderAcompañamiento(pla,i)
                     })}
                     {this.props.getLs[3]().map(b => {
                         return (
-                            <div className='card'
-                                style={{
-                                    zIndex: '0', 
-                                    backdropFilter: 'blur(10px)', 
-                                    backgroundColor:'rgba(179, 241, 178, 0.5)',
-                                    margin: '30px',
-                                    marginTop:'0px',
-                                    marginBottom:'0px'
-                                }}
-                            >
-                                <li span className='nav-text'>
-                                    <span style={{ fontFamily: 'Cinzel' }}> {b.key.nombre}</span>
-                                    <span style={{ fontFamily: 'Cinzel' }}> {b.amount}</span>
-                                    <div style={{overflow: 'auto'}}>
-                                        <button 
-                                            type='button'
-                                            className='btn'
-                                            onClick={() => {
-                                                console.log(this.state)
-                                            }}
-                                            style={{
-                                                color: 'red',
-                                                float:'right'
-                                            }}
-                                        >
-                                            <AiOutlineCloseSquare/>
-                                        </button>
-                                    </div>
-                                </li>
-                            </div>
+                            this.renderBebida(b)
                         )
                     })}
-                </IconContext.Provider>
                 </div>
             </React.Fragment>
         );
